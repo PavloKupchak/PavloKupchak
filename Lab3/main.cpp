7d269fd15399f561b5b3821133fba82b80ca939b
PavloKupchak <PAVLO.KUPCHAK@lnu.edu.ua>
2026-04-14 19:30:53 +0300

    lab3

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     Dyhotomia_class dyh;
-        
+
     double a, b, eps;
 
     cout << endl;
@@ -18,35 +18,36 @@ int main() {
 
     dyh.setTolerance(eps);
 
-    double xDYHOTOMIA;     
-    double xNEWTON;  
+    double xLeft;
+    double xRight;
+    double xNewton;
 
     if (a < 0 && b > 0) {
         cout << "Інтервал проходить через 0\n";
 
-        double delta = 1e-6;
-        dyh.set_volumes(a, -delta);
-        if (dyh.count(xDYHOTOMIA) == 0) {
-            cout << "Лівий корінь (Дихотомія): " 
-                 << fixed << setprecision(6) << xDYHOTOMIA << endl;
+        dyh.set_volumes(a, -1e-6);
+        if (dyh.count(xLeft) == 0) {
+            cout << "Лівий корінь (Дихотомія): "
+                 << fixed << setprecision(6) << xLeft << endl;
         } else {
             cout << "Помилка на лівому інтервалі\n";
         }
 
-        dyh.set_volumes(delta, b);
-        if (dyh.count(xNEWTON) == 0) {
-            cout << "Правий корінь (Дихотомія): " 
-                 << fixed << setprecision(6) << xNEWTON << endl;
+        dyh.set_volumes(1e-6, b);
+        if (dyh.count(xRight) == 0) {
+            cout << "Правий корінь (Дихотомія): "
+                 << fixed << setprecision(6) << xRight << endl;
         } else {
             cout << "Помилка на правому інтервалі\n";
         }
     }
+    
     else {
         dyh.set_volumes(a, b);
 
-        if (dyh.count(xDYHOTOMIA) == 0) {
-            cout << "Дихотомія x = " 
-                 << fixed << setprecision(6) << xDYHOTOMIA << endl;
+        if (dyh.count(xLeft) == 0) {
+            cout << "Дихотомія x = "
+                 << fixed << setprecision(6) << xLeft << endl;
         } else {
             cout << "Помилка дихотомії\n";
         }
@@ -54,10 +55,11 @@ int main() {
 
     cout << "\nМетод Ньютона\n";
     cout << "Введіть початкове наближення x0: ";
-    cin >> xNEWTON;
+    cin >> xNewton;
 
-    if (dyh.newton(xNEWTON) == 0) {
-        cout << "Ньютон x = " << fixed << setprecision(6) << xNEWTON << endl << endl;
+    if (dyh.newton(xNewton) == 0) {
+        cout << "Ньютон x = "
+             << fixed << setprecision(6) << xNewton << endl;
     } else {
         cout << "Помилка Ньютона\n";
     }
